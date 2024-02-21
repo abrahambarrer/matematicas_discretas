b688fd88fe9894588c05cb5a0d3e7ebe43b783ce
Abraham <abrahambarrera072@gmail.com>
2024-02-21 10:36:29 -0600

    actualizacion

--- sucesion_perfectos.py
+++ sucesion_perfectos.py
@@ -1,19 +1,22 @@
 '''
-    este programa determina si un numero n es perfecto
+    este programa calcula los numeros perfectos hasta un numero n
 '''
 
-numero = int(input('Proporciona un numero: '))
-divisores = []
-suma = 0
+def perfecto(numero):
+    divisores = []
+    suma = 0
 
-for i in range(1, numero // 2 + 1):
-    if numero % i == 0:
-        divisores.append(i)
+    for i in range(1, numero // 2 + 1):
+        if numero % i == 0:
+            divisores.append(i)
 
-for i in divisores:
-    suma += i
+    for i in divisores:
+        suma += i
 
-if suma == numero:
-    print(True)
-else:
-    print(False)
+    if suma == numero:
+        print(numero)
+
+numero = int(input('Proporciona numero: '))
+
+for i in range(2, numero + 1):
+    perfecto(i)
